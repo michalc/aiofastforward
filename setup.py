5103d50100a798b27f973a2fa0f2153b9449d306
Michal Charemza <michal@charemza.name>
2018-12-15 15:51:34 -0500

    (release) v0.0.14

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='aiofastforward',
-    version='0.0.13',
+    version='0.0.14',
     author='Michal Charemza',
     author_email='michal@charemza.name',
     description='Fast-forward time in asyncio Python by patching loop.time, loop.call_later, loop.call_at, and asyncio.sleep',
